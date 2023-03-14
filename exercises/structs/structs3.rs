b7592525f07cdf9c5b1f4be37701b233676a3eb0
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-14 13:45:44 -0700

    small update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -27,13 +27,18 @@ impl Package {
     }
 
     fn is_international(&self) -> bool {
-        match self {
-            
+        match self{
+           self.recipient_country => {
+               false
+           }
+           _ => {
+                true
+           }
         }
     }
 
     fn get_fees(&self, cents_per_gram: i32) -> i32 {
-        // Something goes here...
+        self.weight_in_grams * cents_per_gram
     }
 }
 
